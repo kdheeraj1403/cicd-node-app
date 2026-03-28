681b25fefa516642e6102f0dd5ea608086c81786
Deeraj <kdheeraj1403@gmail.com>
2026-03-28 17:12:03 +0530

    fix ecr image issue

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Deployment with AWS ECR is Success 🚀');
+  res.send('Deployment with AWS ECR is in Success 🚀');
 });
 
 app.listen(port, () => {
