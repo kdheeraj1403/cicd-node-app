130f481acf6a4fefef7a0ed79231512219912b62
Deeraj <kdheeraj1403@gmail.com>
2026-03-31 19:10:12 +0530

    fic binding for ALB

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
+
 const app = express();
+
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Push image to AWS ECR is Success 🚀');
+  res.send('ASG + ALB working 🚀');
 });
 
 app.listen(port, "0.0.0.0", () => {
-  console.log(`App is running on port ${port}`);
+  console.log(`App running on port ${port}`);
 });
