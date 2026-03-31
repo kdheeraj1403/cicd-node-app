7a60cb4df26da69b03d4777de6366fcf276b9779
Deeraj <kdheeraj1403@gmail.com>
2026-03-31 18:31:33 +0530

    remove deploy to EC2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Deployment with AWS ECR is Success 🚀');
+  res.send('Push image to AWS ECR is Success 🚀');
 });
 
 app.listen(port, "0.0.0.0", () => {
